b179a3d33405a4f8cb56459ece4d9b8633d8f776
IT21810732 <it21810732@my.sliit.lk>
2024-04-23 01:25:36 +0530

    Create User Model

---
+++ backend/models/User.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    type: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        required: true
+    },
+    first_name: {
+        type: String,
+        required: true
+    },
+    last_name: {
+        type: String,
+        required: true
+    },
+    dob: {
+        type: Date,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true
+    },
+    mobile_number: {
+        type: Number,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: false
+    }
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
