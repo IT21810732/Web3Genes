91b37c7b825b8101b2ae5038bf846a748ee09df3
IT21810732 <it21810732@my.sliit.lk>
2024-04-23 01:45:55 +0530

    Create backend routes for Read, Update and Delete

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -25,6 +25,58 @@ router.route("/add").post((req, res) => {
         });
 });
 
+// Route to get all users
+router.route("/").get((req, res) => {
+    User.find()
+        .then((users) => res.json(users))
+        .catch((err) => {
+            console.log(err);
+            res.status(400).json("Error: " + err);
+        });
+});
 
+// Route to get user by ID
+router.route("/:id").get((req, res) => {
+    User.findById(req.params.id)
+        .then((user) => res.json(user))
+        .catch((err) => {
+            console.log(err);
+            res.status(400).json("Error: " + err);
+        });
+});
+
+// Route to update user by ID
+router.route("/update/:id").put((req, res) => {
+    User.findById(req.params.id)
+        .then((user) => {
+            user.type = req.body.type;
+            user.status = req.body.status;
+            user.first_name = req.body.first_name;
+            user.last_name = req.body.last_name;
+            user.dob = req.body.dob;
+            user.gender = req.body.gender;
+            user.mobile_number = req.body.mobile_number;
+            user.email = req.body.email;
+            user.password = req.body.password;
+
+            user.save()
+                .then(() => res.json("User updated"))
+                .catch((err) => res.status(400).json("Error: " + err));
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(400).json("Error: " + err);
+        });
+});
+
+// Route to delete user by ID
+router.route("/delete/:id").delete((req, res) => {
+    User.findByIdAndDelete(req.params.id)
+        .then(() => res.json("User deleted"))
+        .catch((err) => {
+            console.log(err);
+            res.status(400).json("Error: " + err);
+        });
+});
 
 module.exports = router;
